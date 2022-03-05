af4fb6875d8942abb119f2a207496fe7b13e6be0
Liz <adhiamboliz3@gmail.com>
2022-03-05 16:25:53 +0300

    added a card for results

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var currentDate = new Date();
 var currentYear = currentDate.getFullYear();
 
 function giveName() {
-  var name = document.getElementById("yourNames").value;
+  var name = document.getElementById("yName").value;
   if (name == "") {
-    alert("Name must be filled out");
+    alert("Please fill in your name");
     return false;
   }
 }
@@ -12,31 +12,23 @@ function giveName() {
 function selectGender() {
   var gender = document.getElementById("gender").value;
   if (gender == "1") {
-    alert("Gender must be selected");
-    return false;
-  }
-}
-
-function giveCentury() {
-  var century = document.getElementById("centuryBorn").value;
-  if (century == "") {
-    alert("Century must be filled out");
+    alert("Please select Gender");
     return false;
   }
 }
 
 function giveYear() {
-  var year = document.getElementById("yearBorn").value;
+  var year = document.getElementById("yBorn").value;
   if (year == "") {
-    alert("Year must be filled out");
+    alert("Please fill in your year of Birth");
     return false;
   }
 }
 
 function giveMonth() {
-  var month = document.getElementById("monthBorn").value;
+  var month = document.getElementById("mBorn").value;
   if (month == "0") {
-    alert("Month must be selected");
+    alert("Please select month");
     return false;
   }
 }
@@ -44,7 +36,7 @@ function giveMonth() {
 function giveDay() {
   var day = document.getElementById("dayBorn").value;
   if (day == "") {
-    alert("Day must be filled out");
+    alert("Please fill in the day you were born");
     return false;
   }
 }
@@ -59,48 +51,13 @@ function digits(input){
   input.value = input.value.replace(digits, "");
 }
 
-function validYear(){
-  var year = document.getElementById('yearBorn').value;
-  if (year < 1899 || year > currentYear) {
-    alert("Invalid Year");
-    return false;
-  }
-}
-
-function validDay(){
-  var thirtyOneMonths = [1, 3, 5, 7, 9, 10, 12];
-  var monthNumber = parseInt(document.getElementById("monthBorn").value);
-  var dayNumber = parseInt(document.getElementById("dayBorn").value);
-  var yearNumber = parseInt(document.getElementById("yearBorn").value);
-  var yearNumber = parseInt(document.getElementById("centuryBorn").value);
-  var a = yearNumber % 100;
-  var b = yearNumber % 400;
-  var c = yearNumber % 4;
-  var d = thirtyOneMonths.includes(monthNumber);
-  if (monthNumber === 2 && dayNumber > 28 && a === 0 && b !== 0){
-    alert("Invalid day: The entered year, February had 28 days.");
-    return false;
-  }
-  else if (monthNumber === 2 && dayNumber > 28 && c !== 0) {
-    alert("Invalid day: The entered year, February had 28 days.");
-    return false;
-  }
-  else if (!d && dayNumber > 30) {
-    alert("Invalid day: The selected month has 30 days");
-    return false;
-  }
-  else if (dayNumber > 31 || dayNumber < 1) {
-    alert("Invalid day: Months have a possible 1 to 31 days");
-    return false;
-  }
-}
 
 function getName(){
   var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
   var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-  var cc = parseInt(document.getElementById("centuryBorn").value.slice(0, 2));
-  var yy = parseInt(document.getElementById("yearBorn").value.slice(2, 4));
+  var cc = parseInt(document.getElementById("yBorn").value.slice(0, 2));
+  var yy = parseInt(document.getElementById("yBorn").value.slice(2, 4));
   var dd = parseInt(document.getElementById("dayBorn").value);
   var mm = parseInt(document.getElementById("monthBorn").value);
   var day = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
